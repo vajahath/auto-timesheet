5d7ec5628457083b88b08e962c16b94e81d276bc
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-06 10:49:41 +0530

    added authenticity_token extractor

---
+++ src/token-extractor.js
@@ -0,0 +1,18 @@
+// module that extracts authenticity_token from the page
+const cheerio = require('cheerio');
+const request = require('request');
+const Promise = require('bluebird');
+
+module.exports = () => {
+	return new Promise((resolve, reject) => {
+		request('http://projects.cubettech.com/login?back_url=http%3A%2F%2Fprojects.cubettech.com%2F', (err, res, body) => {
+			if (err) reject(err);
+			else {
+				let page = cheerio.load(body);
+				let authenticityToken = page('#login-form input')[1].attribs.value;
+				if (!authenticityToken) reject(new Error('Failed to get authenticity_token. Please rise an issue on github..'))
+				else resolve(authenticityToken);
+			}
+		})
+	})
+}
