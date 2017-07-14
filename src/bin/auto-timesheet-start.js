fab731f667957476b74b2a3190f9cbaa48191298
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-14 16:43:57 +0530

    integrating update-available addressing #15 and #16

--- src/bin/auto-timesheet-start.js
+++ src/bin/auto-timesheet-start.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const app = require('../periodic');
+const checkForUpdates = require('../utils/check-for-updates');
 
 console.log('auto-timesheet starting');
+
 app();
+checkForUpdates();
