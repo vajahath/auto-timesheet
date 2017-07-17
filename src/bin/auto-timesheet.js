589225c68ec2f5405d291b42269d5b55786718b8
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-17 06:16:44 +0530

    automatically fetches app-version

--- src/bin/auto-timesheet.js
+++ src/bin/auto-timesheet.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const getVersion = require('installed-version');
 
 program
-    .version('0.0.31')
-    .command('cred', 'configure stuff')
+    .version(getVersion('auto-timesheet'))
+    .command('cred', 'set credentials')
     .command('conf', 'configure stuff')
-    .command('start', 'configure stuff')
+    .command('start', 'start application')
     .parse(process.argv);
