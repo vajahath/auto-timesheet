96b77cf10bb9004e6f6bf316b28f06729c938455
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-08-04 15:16:18 +0530

    changing hash tags to star tags # to * addressing #13

--- src/git-handlers/tag-extractor.js
+++ src/git-handlers/tag-extractor.js
@@ -1,16 +1,16 @@
 /*eslint no-useless-escape:0*/
 
 /**
- * Function to extract #tags 
+ * Function to extract *tags 
  */
 function tagExtractor(msg) {
 	let issue;
-	issue = msg.match(/\#\w\w+\b/g) || null;
+	issue = msg.match(/\*\w\w+\b/g) || null;
 	if (!issue) return null;
 
 	let random = getRandomInt(0, issue.length - 1);
 	let tag = issue[random];
-	tag = tag.replace('#', '');
+	tag = tag.replace("*", "");
 	return tag;
 }
 
@@ -21,6 +21,5 @@ function tagExtractor(msg) {
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 module.exports = tagExtractor;
--- tests/git-handlers/tag-extractor.spec.js
+++ tests/git-handlers/tag-extractor.spec.js
@@ -1,22 +1,22 @@
 /*eslint no-undef:0 */
 
-const expect = require('chai').expect;
-const tagExtractor = require('../../src/git-handlers/tag-extractor');
+const expect = require("chai").expect;
+const tagExtractor = require("../../src/git-handlers/tag-extractor");
 
-describe('testing tag extractor >', () => {
-	it('should successfully extract tags of sample string 1', done => {
-		let s = 'hello from the other side.. ti#hello hello, hhhh #tt dfhgkjdh';
-		expect(tagExtractor(s)).to.be.oneOf(['hello', 'tt']);
+describe("testing tag extractor >", () => {
+	it("should successfully extract tags of sample string 1", done => {
+		let s = "hello from the other side.. ti*hello hello, hhhh *tt dfhgkjdh";
+		expect(tagExtractor(s)).to.be.oneOf(["hello", "tt"]);
 		done();
 	});
 
-	it('should successfully extract tags of sample string 2', done => {
-		let s = 'hello from the other side.. ti#hello., hello';
-		expect(tagExtractor(s)).to.equal('hello');
+	it("should successfully extract tags of sample string 2", done => {
+		let s = "hello from the other side.. ti*hello., hello";
+		expect(tagExtractor(s)).to.equal("hello");
 		done();
 	});
-	it('should return null if nothing found', done => {
-		let s = 'hello from the other side.. hello., hello';
+	it("should return null if nothing found", done => {
+		let s = "hello from the other side.. hello., hello";
 		expect(tagExtractor(s)).to.be.null;
 		done();
 	});
